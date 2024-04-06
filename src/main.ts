520d1a71c9bb1d809eec7dbdafbcf09346fac1e8
Carlos Ramirez <carlos@extrasistemas.com>
2024-04-05 18:42:48 -0600

    feat: added prisma orm for databases in sqlite

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
+import { read } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Main');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen(envs.port);
+  logger.log(`Server running on http://localhost:${envs.port}`);
 }
 bootstrap();
